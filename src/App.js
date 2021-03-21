15be5441a93caa900b16faf3561934c4766c7d0d
Ammm4 <ammm.4amit@gmail.com>
2021-03-21 12:14:13 +0000

    login section complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 
+import {React, useState} from 'react';
+import SignInForm from './components/signInForm/signIn'
 import './App.css';
 
+
 function App() {
+  const users = [
+                 {
+                   username:'Monk', email: 'amit@cardiff.com', password: 'tansen123', online: true, friends:['Fighter','Warrior']
+                 },
+                 {
+                 username:'Fighter', email: 'suraj@helsinki.com', password: 'dhamkada123', online: true, friends:['Monk','Warrior']
+                 },
+                 {
+                  username:'Warrior', email: 'lok@london.com', password: 'jhapa123', online: true, friends:['Monk','Fighter']
+                 }
+  ];
+  const [user, setUser] = useState({name:"", email:""});
+
+  const [error, setError] = useState("");
+
+  const signIn = (logInDetails) => {
+    users.map(user => {
+         if (logInDetails.email === user.email && logInDetails.password === user.password){
+           console.log('You have Signed up!!')
+         } else {
+           setError("Incorrect email or password!")
+         }
+    })
+  }
+
+  const signOut = () => {
+   console.log("Bye!!")
+  }
+
   return (
     <div className="App">
-       <h1> Hello</h1>
+       <SignInForm signIn={signIn} error={error}/>
     </div>
   );
 }
---
+++ src/components/signInForm/signIn.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import './signIn.css';
+
+
+function SignInForm({signIn, error}){
+ const [signInDetails, setSignInDetails] = useState({email:"", password:""})
+
+ const handleSubmit = (event) => {
+       event.preventDefault();
+       signIn(signInDetails)
+ }
+  return (
+    <div className="signInPage">
+      <h1>tic-tac-toe</h1>
+      <form onSubmit={handleSubmit}>
+            <div className="form-heading">
+                <h2>Log In</h2>
+            </div>
+            <div className="form-inputs">
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" id="email" onChange={e => setSignInDetails({...signInDetails, email: e.target.value})} value={setSignInDetails.email}/>              
+            </div> 
+            <div className="form-inputs">
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" id="password" onChange={e => setSignInDetails({...signInDetails, password: e.target.value})} value={setSignInDetails.password}/>
+            </div> 
+            
+            <input type="submit" value="Sign In"/>
+            <p>Don't have an account? <a href="#">Sign Up</a></p>  
+            <p className="error">{error}</p> 
+      </form>
+      
+    </div>
+  )
+}
+
+export default SignInForm;
