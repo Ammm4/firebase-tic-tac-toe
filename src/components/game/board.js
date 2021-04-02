0addee52a299a1c6e88bd83931a69ef3cf745881
Ammm4 <ammm.4amit@gmail.com>
2021-04-02 11:21:27 +0100

    winningLine animation added

--- src/components/game/board.js
+++ src/components/game/board.js
@@ -1,25 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Square from './cells';
-import useCheck from './useCheck';
+import Lines from './lines';
 
-const Board = ({squares, handleClick, currentTurn}) => {
+
+const Board = ({squares, handleClick, currentTurn, winningLine}) => {
 
  const clickhandler = (index) => {
       handleClick(index)
  }
-
   return (
        <div className={currentTurn? 'game-board o-turn':'game-board x-turn'} id="game-board">
               {squares.map((squareStyle,index) => {
                 return (<Square key={index} squareStyle={squareStyle} clickhandler={() => clickhandler(index)}/>)
               })
               }
-    
-          </div>)
-          
-        
-  }
-
-
+                       
+            { <Lines winningLine={winningLine}/>}
+        </div>
+        )                 
+}
 
 export default Board;
