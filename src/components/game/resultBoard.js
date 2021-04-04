9230758cfbd8ba9cc271e3bc0d4d56e96f7d07d4
Ammm4 <ammm.4amit@gmail.com>
2021-04-04 11:07:54 +0100

    padding added and fontawesome linked

--- src/components/game/resultBoard.js
+++ src/components/game/resultBoard.js
@@ -4,7 +4,7 @@ const ResultBoard = ({user, winner, playAgain}) => {
     return (<div className="score-area">
             <h6>Game Over</h6>
             {(winner !== 'Draw')?
-                   <div className="results blink">{winner === user.username? 'You Win!': `${winner} wins!` }</div>:
+                   <div className="results blink"><i class="fas fa-trophy"></i> {winner === user.username? 'You Win!': `${winner} wins!` }</div>:
                    <div className="results blink">Match Draw!</div>  
                 }          
              <button onClick={() => playAgain()}>Play Again</button>
