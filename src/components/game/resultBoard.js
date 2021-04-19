f69c5b503cdd0cdffbd35ef44472dd2fb2619c0e
Ammm4 <ammm.4amit@gmail.com>
2021-04-19 22:23:41 +0100

    game request out display component added

--- src/components/game/resultBoard.js
+++ src/components/game/resultBoard.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ResultBoard = ({user, winner, playAgain}) => {
     return (<div className="score-area">
             <h6>Game Over</h6>
-            {(winner !== 'Draw')?
-                   <div className="results blink"><i class="fas fa-trophy"></i> {winner === user.username? 'You Win!': `${winner} wins!` }</div>:
+            {(winner !== 'draw')?
+                   <div className="results blink"><i className="fas fa-trophy"></i> {winner === user.username? 'You Win!': `${winner} wins!` }</div>:
                    <div className="results blink">Match Draw!</div>  
                 }          
              <button onClick={() => playAgain()}>Play Again</button>
