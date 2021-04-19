6675f0b393997c5dd21a2c89cb4c1a83beee7568
Ammm4 <ammm.4amit@gmail.com>
2021-04-19 22:08:47 +0100

    scoreboard section add

---
+++ src/components/game/scoreboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Scoreboard = ({username, opponent,score}) => {
+  return(
+    <div className="scoreboard">
+      <h2>Score</h2>
+      <div className="score-details">
+        <h5>You : {score[username]}</h5>
+        <h5>{opponent} : {score[opponent]}</h5>
+        <h5>Draw : {score.draw}</h5>
+      </div>   
+    </div>
+  )
+}
+
+export default Scoreboard;
