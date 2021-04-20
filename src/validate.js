41e85d415454fe06b4ee36282aeaf12940900304
Ammm4 <ammm.4amit@gmail.com>
2021-04-20 11:26:59 +0100

    form validation code done

---
+++ src/validate.js
@@ -0,0 +1,25 @@
+
+export const validate = (userCredentials) => {
+   let errors = {};
+   if(!userCredentials.username){
+     errors.username = "Username Required"
+   }
+
+   if(!userCredentials.email){
+     errors.email = "Email Required"
+   } else if(!/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(userCredentials.email)){
+     errors.email = "Email Invalid"
+   }
+  
+   if(!userCredentials.password){
+    errors.password = "Password Required"
+   }else if (userCredentials.password.length < 6){
+    errors.password = "Password too short"
+   }
+   if(!userCredentials.password1){
+    errors.password1 = "Password Required"
+   } else if(userCredentials.password1 !== userCredentials.password){
+    errors.password1 = "Passwords do not match"
+   }
+   return errors
+}
